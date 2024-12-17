70d08ac8f87f06dbf43be69bf31889e771938133
Behzad Mousavi <behzad.mousavi@snapp.cab>
2024-12-17 16:57:53 +0330

    init project

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
-	"docking-in/internal"
 	"github.com/labstack/echo/v4"
+	"net/http"
 )
 
 func main() {
 	e := echo.New()
 
-	internal.RegisterRoutes(e)
-
+	v1 := e.Group("v1/api")
+	{
+		v1.GET("/intro", GetIntro)
+	}
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func GetIntro(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status":  http.StatusOK,
+		"message": "Ok",
+		"data":    "Mini application to get along with docker",
+	})
 }
